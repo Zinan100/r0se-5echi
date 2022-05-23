7340745662db6119e4e1e081978b406668bcb90a
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-23 13:02:46 +0530

    Create Start.sh

---
+++ Start.sh
@@ -0,0 +1,11 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Zinan100/ADV-GUARDIAN-GROOT.git /ADV-GUARDIAN-GROOT
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /ADV-GUARDIAN-GROOT
+fi
+cd /ADV-GUARDIAN-GROOT
+pip3 install -U -r requirements.txt
+echo "Starting GROOT....
